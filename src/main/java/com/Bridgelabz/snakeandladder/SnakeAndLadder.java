debfba56c33798e14b5be7791866076097d37ee1
akshay <akshay.yamgar29@gmail.com>
2021-10-04 12:03:12 +0530

    Added: Initialized start position

--- src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,8 +1,14 @@
 package com.Bridgelabz.snakeandladder;
 
-
+/**
+ * Snake And Ladder Simulator
+ */
 public class SnakeAndLadder {
     public static void main(String[] args) {
+        // Constant variable
+        int INITIAL_POSITION = 0;
+        int NUMBER_OF_PLAYER = 1;
+
         System.out.println("Welcome to Snake And Ladder Game!!!");
     }
 }
