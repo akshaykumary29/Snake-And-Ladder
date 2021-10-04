a3a5856c2a69e295a5323e20e1b912b9c3673444
akshay <akshay.yamgar29@gmail.com>
2021-10-04 14:37:29 +0530

    Added: Random number is added for roll die

--- src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,15 +1,18 @@
 package com.Bridgelabz.snakeandladder;
 
+
 /**
  * Snake And Ladder Simulator
  */
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
         // Constant variable
         int INITIAL_POSITION = 0;
-        int NUMBER_OF_PLAYER = 1;
+        int NUMBER_OF_PLAYER = 0;
 
         System.out.println("Welcome to Snake And Ladder Game!!!");
+        int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
     }
 }
 
