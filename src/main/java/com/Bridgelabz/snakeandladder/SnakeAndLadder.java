055cd1291423e1cf0fce54945c61a9ff78fc5a24
akshay <akshay.yamgar29@gmail.com>
2021-10-04 15:29:59 +0530

    Added: Code for two player

--- src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,49 +4,71 @@ package com.Bridgelabz.snakeandladder;
  * Snake And Ladder Simulator
  */
 public class SnakeAndLadder {
+    // Constant
+    public static final int IS_SNAKE = 1;
+    public static final int IS_LADDER = 2;
+    public static final int WINNING_POSITION = 100;
+
+    static int diceCount = 0;
+    static String turn = "player1";
 
     public static void main(String[] args) {
-        // Constant variable
-        int INITIAL_POSITION = 0;
-        int NUMBER_OF_PLAYER = 1;
-        int IS_SNAKE = 1;
-        int IS_LADDER = 2;
-        int WINNING_POSITION = 100;
-        int diceCount = 0;
-        int position = INITIAL_POSITION;
+        System.out.println("Welcome to snake and ladder project");
+        // initial player position
+        int player1Position = 0;
+        int player2Position = 0;
+        int player1DiceCount = 0;
+        int player2DiceCount = 0;
 
-        System.out.println("Welcome to Snake And Ladder Game!!!");
-
-        while (position < WINNING_POSITION) {
-            diceCount++;
-
-            int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-
-            int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
-
-            // checks for option
-            if (option == IS_SNAKE) {
-                System.out.println("Snake for the Player");
-//                position -= rollDie;
-//                if (position < 0) {
-//                    position = 0;
-                if (position - rollDie > 0) {
-                    position -= rollDie;
-                }
-            } else if (option == IS_LADDER) {
-                System.out.println("Ladder for the Player");
-                position += rollDie;
-                if (position > 100) {
-                    position -= rollDie;
-                }
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
+            ++diceCount;
+            if (turn.equals("player1")) {
+                turn = "player2";
+                ++player1DiceCount;
+                player1Position = getPosition(player1Position);
             } else {
-                System.out.println("No Play for Player");
+                ++player2DiceCount;
+                turn = "player1";
+                player2Position = getPosition(player2Position);
             }
-            System.out.println("Dice: " + rollDie);
-            System.out.println("Dice count: " + diceCount);
-            System.out.println("Position: " + position);
-
         }
+
+        if (player1Position == WINNING_POSITION) {
+            System.out.println("Player1 win the game");
+            System.out.println("dice count " + player1DiceCount);
+        } else {
+            System.out.println("Player2 win the game");
+            System.out.println("dice count " + player2DiceCount);
+        }
+        System.out.println("Total dice count " + diceCount);
+
+        System.out.println("player1Position " + player1Position);
+        System.out.println("player2Position " + player2Position);
+
+    }
+
+    private static int getPosition(int position) {
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        int option = (int) (Math.floor(Math.random() * 10) % 3);
+        if (option == IS_SNAKE) {
+            System.out.println("Snake for the player");
+            if (position - diceNumber > 0) {
+                position -= diceNumber;
+            }
+        } else if (option == IS_LADDER) {
+            System.out.println("Ladder for the player");
+            if (turn.equals("player1")) {
+                turn = "player1";
+            } else if (turn.equals("player2")) {
+                turn = "player2";
+            }
+            if (position + diceNumber <= 100) {
+                position += diceNumber;
+            }
+        } else {
+            System.out.println("No play for the player");
+        }
+        return position;
     }
 }
 
