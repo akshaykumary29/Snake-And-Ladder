2b812ae13b067df131ac4562fcbe5b5b98147e10
akshay <akshay.yamgar29@gmail.com>
2021-10-04 15:04:09 +0530

    Added: Repeating till player reaches 100 position

--- src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/Bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -12,25 +12,33 @@ public class SnakeAndLadder {
         int NUMBER_OF_PLAYER = 1;
         int IS_SNAKE = 1;
         int IS_LADDER = 2;
+        int WINNING_POSITION = 100;
+        int position = INITIAL_POSITION;
 
         System.out.println("Welcome to Snake And Ladder Game!!!");
-        int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 
-        int option = (int) (Math.floor(Math.random()* 10) % 2 + 1);
+        while (position < WINNING_POSITION) {
 
-        int position = INITIAL_POSITION;
-        // checks for option
-        if (option == IS_SNAKE) {
-            System.out.println("Snake for the Player");
-            position -= rollDie;
-        }else if (option == IS_LADDER) {
-            System.out.println("Ladder for the Player");
-            position += rollDie;
-        }else {
-            System.out.println("No Play for Player");
+            int rollDie = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+
+            int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
+
+            // checks for option
+            if (option == IS_SNAKE) {
+                System.out.println("Snake for the Player");
+                position -= rollDie;
+                if (position < 0) {
+                    position = 0;
+                }
+            } else if (option == IS_LADDER) {
+                System.out.println("Ladder for the Player");
+                position += rollDie;
+            } else {
+                System.out.println("No Play for Player");
+            }
+            System.out.println("Position: " + position);
+
         }
-        System.out.println("Position: " + position);
-
     }
 }
 
